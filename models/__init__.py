a27742def88a1025c4ccea588ff24c2806637ac9
mohamedessamx <me8624708@gmail.com>
2023-12-10 09:44:35 -0800

    l

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Importing FileStorage class"""
-
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
