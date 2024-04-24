9cd41ec9c768fad463aac3d6e9f3775d408f5d5b
rodynaamrfathy <rodynaamr@icloud.com>
2024-04-24 04:45:28 -0700

    ‘UPDATE’

--- console.py
+++ console.py
@@ -1,26 +1,17 @@
+#!/usr/bin/python3
 import cmd
 import re
 from shlex import split
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    intro = "welcome to my airbnb clone"
-
     """Defines the HolbertonBnB command interpreter.
 
     Attributes:
         prompt (str): The command prompt.
     """
 
-    prompt = ("hbnb")
+    prompt = "(hbnb) "
 
     def emptyline(self):
         """Do nothing upon receiving an empty line."""
@@ -32,7 +23,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """EOF signal to exit the program."""
-        print("BYE")
+        print("")
         return True
 
 if __name__ == '__main__':
